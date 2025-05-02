6805447cdeb43ad610f9962fe1310cbbd96a571c
parsard <parsaroodhastam@gmail.com>
2025-05-02 12:22:49 +0330

    assets updated,folders created.

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static final HexColor green_50 = HexColor('#f3f5f2');
+  static final HexColor green_100 = HexColor('#dadfd5');
+  static final HexColor green_200 = HexColor('#c9d0c1');
+  static final HexColor green_300 = HexColor('#b0baa5');
+  static final HexColor green_400 = HexColor('#a1ad93');
+  static final HexColor green_500 = HexColor('#899878');
+  static final HexColor green_600 = HexColor('#7d8a6d');
+  static final HexColor green_700 = HexColor('#616c55');
+  static final HexColor green_800 = HexColor('#4b5442');
+  static final HexColor green_900 = HexColor('#3a4032');
+  static final HexColor babyPowder = HexColor('#F7F7F2');
+  static final HexColor beige = HexColor('#E4E6C3');
+  static final HexColor erieBlack = HexColor('#222725');
+}
